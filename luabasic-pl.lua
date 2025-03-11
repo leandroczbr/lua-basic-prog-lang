b6e3cc65cc1fb8e129825dae0fdfb11b85e95c24
Leandro C. W. Mattos <96585021+leandroczbr@users.noreply.github.com>
2025-03-11 17:49:18 -0300

    Update luabasic-pl.lua

--- luabasic-pl.lua
+++ luabasic-pl.lua
@@ -290,32 +290,6 @@ end
 
 comlua.fvar = fvar
 comlua.fvars = fvars
----------- testing with fibonacci sequence
-
-    comlua.run(
-    [[
-    set first 0;
-    set second 1;
-    set next 0;
-    add next first second;
-    print first;
-    print second;
-    print next;
-    
-    set index 0;
-    set max 10;
-    set continue true;
-    :loop
-        set first second;
-        set second next;
-        set next 0;
-        add next first second;
-        print next;
-        add index 1;
-        big continue max index;
-    ifgt continue loop;
-    
-    ]])
 
 -------
 return comlua
